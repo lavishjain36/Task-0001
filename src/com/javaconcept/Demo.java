c096583719d853f2e1d475d397cd4fcbcee08c52
Lavish <lavishjain36@gmail.com>
2024-07-03 13:01:40 +0530

    first commit

---
+++ src/com/javaconcept/Demo.java
@@ -0,0 +1,13 @@
+package com.javaconcept;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		//main method.
+	 System.out.println("Hello buddy.How are you today?");
+	}
+}
+
+
+//Output
+//Hello buddy.How are you today?
---
+++ src/com/javaconcept/Test.java
@@ -0,0 +1,40 @@
+package com.javaconcept;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static  void main(String args[]) {
+		
+	     
+	
+				
+//		String s1="Guvi";
+//		String s2=new String("Guvi");
+//		
+//
+//		System.out.println(s1==s2);//false
+//		System.out.println(s1.equals(s2));//true
+		
+//		StringBuffer s1=new StringBuffer("Lavish");
+//		StringBuffer s2=new StringBuffer("Lavish");
+		
+//		System.out.println(s1.hashCode());
+//		System.out.println(s2.hashCode());
+//
+//		System.out.println(s1==s2);//false
+//		System.out.println(s1.equals(s2));//false->value of address of the object
+		
+		StringBuilder s1=new StringBuilder("Lavish");
+		StringBuilder s2=new StringBuilder("Lavish");
+		System.out.println(s1);
+		System.out.println(s2);
+
+
+	}
+}
+
+
+//Output:
+//Lavish
+//Lavish
